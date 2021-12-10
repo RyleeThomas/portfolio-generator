5e71485a83ce240d92889ff43f16af2255d1ca7f
rylee thomas <ryleetreat@gmail.com>
2021-12-09 23:24:29 -0500

    updated the quesitons to validate user input

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ const promptUser = () => {
     {
         type: 'input',
         name: 'github',
-        message: 'Enter your Github Username'
+        message: 'Enter your Github Username (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please your Github Username!');
+              return false;
+            }
+        }
     },
 
     {
@@ -46,12 +54,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required) ',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter name of your project!');
+              return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please provide a description of project!');
+              return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -62,7 +86,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please provide a Github link!');
+              return false;
+            }
+        }
       },
       {
         type: 'confirm',
